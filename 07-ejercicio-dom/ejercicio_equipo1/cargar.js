ab577b7810d6c5615a442d28c89d8c8217f9fb04
Alfonso Tienda <atienda@iprocuratio.com>
2020-03-30 08:57:49 +0200

    ejercicios updated

---
+++ 07-ejercicio-dom/ejercicio_equipo1/cargar.js
@@ -0,0 +1,92 @@
+//si en el módulo se definió como export default 
+//se debe importar sin llaves
+import pizzas from '../pizzas-content.js';
+
+/* pizzas.forEach(
+    (pizza) => {
+    console.log(pizza);
+});
+ */
+
+ //window.pizzas = pizzas;
+
+
+
+const createTable=()=>{
+    const tabla=document.createElement("table");
+    const divTabla=document.getElementById("tabla");
+    divTabla.appendChild(tabla);
+    tabla.appendChild(_headerTable());
+    _dataTable(pizzas).forEach(element => {
+        
+        tabla.appendChild(element);
+    });
+    tabla.style.border="1px solid black";
+    tabla.style.width= "100%";
+    
+    return tabla;
+}
+
+
+const _dataTable = (pizzas) => {
+    const arrayrows = [];
+    pizzas.forEach(element => {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.innerText= element.id;
+        td1.style.border = "1px dotted blue";
+        const td2 = document.createElement("td");
+        td2.innerText = element.nombre;
+        td2.style.border = "1px dotted blue";
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        arrayrows.push(tr);
+    });
+    return arrayrows;
+}
+
+
+const cualesmitabla=()=>{
+    console.log(window.tabla);
+}
+
+// debug
+window.createTable = createTable;
+
+
+const elemento = () => {
+    const a = document.createElement("a");
+    a.href = "http://www.google.com";
+    a.innerText="Mi enlace";
+    return a;
+}
+
+
+const _headerTable=()=>{
+    const tr_header=document.createElement("tr");
+    const th_id=document.createElement("th");
+    th_id.innerText='id';
+    const th_nombre=document.createElement("th");
+    th_nombre.innerText='nombre';
+    tr_header.appendChild(th_id);
+    tr_header.appendChild(th_nombre);
+
+    return tr_header;
+}
+
+
+// debug mode
+window._headerTable = _headerTable;
+
+ 
+const tabla=()=>{
+
+}
+
+const renderElement = () => {
+    createTable();
+}
+
+export const init = () => {
+    document.querySelector("body").addEventListener("load", renderElement());
+}
---
+++ 07-ejercicio-dom/ejercicio_equipo3/script.js
@@ -0,0 +1,37 @@
+import pizzas from "../pizzas-content.js";
+
+
+const _createHeader = () => {
+  const tr = document.createElement("tr");
+  const thid = document.createElement("th");
+  thid.innerText = "ID";
+  const thnombre = document.createElement("th");
+  thnombre.innerText = "Nombre";
+  thnombre.style.color = "blue";
+
+  tr.appendChild(thid);
+  tr.appendChild(thnombre)
+
+  return tr;
+}
+
+// modo debug
+window._createHeader = _createHeader;
+
+
+const createTable=()=>{
+  const tabla=document.createElement("table");
+  tabla.setAttribute('id','miTabla');
+
+
+
+  const divTabla=document.getElementById("tabla");
+
+  divTabla.appendChild(tabla);
+
+  const miTabla=document.getElementById("miTabla");
+  miTabla.appendChild(_headerTable());
+
+
+}
+
